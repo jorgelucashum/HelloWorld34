6cbce62d622dd4700f4e956393f7a1a664afb8aa
Jorge Lucas <jorgelucashum@gmail.com>
2021-04-22 21:34:17 -0300

    Documentação e matéria finalizada.

--- HelloWorld34/Program.cs
+++ HelloWorld34/Program.cs
@@ -17,11 +17,33 @@ namespace HelloWorld34
     {
         // Estrutura try-catch:
         // Bloco 'try { }': Contém o código que representa a execução normal do trecho do código que pode acarretar em uma exceção.
-
         // Bloco 'catch(Exception e) { }': Contém o codigo a ser executado caso uma exceção ocorra, deve ser especificado o tipo da exceção a ser tratada (upcasting é permitido).
-        static void Main(string[] args)   
+        // Bloco 'finally { }': Contém o código a ser executado independente de ter ocorrido ou não uma exceção. (Exemplo comum: fechar um arquivo ou conexão de banco de dados ao final do processamento).
+        static void Main(string[] args)
         {
+            try // 'try' tentando executar o código
+            {
+                Console.WriteLine("Digite 2 números para realizar uma divisão:");
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
 
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            // 'catch()' é executado caso ocorra alguma exceção, assim "capturando" ela.
+            // É preferivel sempre especificar o máximo possível o tipo da exceção dentro do parâmetro 'catch()', sendo '(Exception)' o tipo mais genérico, servindo pra qualquer exceção.
+            catch (DivideByZeroException e) // Cógido executado caso ocorra uma divisão por zero (exceção). 
+            {
+                Console.WriteLine("Não é possível realizar uma divisão por zero.");
+            }
+            catch(FormatException e) // Cógido executado caso a entrada dos valrores estejam fora do formato aceitado (exceção). 
+            {
+                Console.WriteLine("Erro no formato. " + e.Message); // '.Message' retorna a própria mensagem padrão criada pela classe da exceção utilizada.
+            }
+            finally // Executando de forma independente de ter ou não ocorrida uma exceção.
+            {
+                Console.WriteLine("\nO código executado com exito!");
+            }
         }
     }
 }
